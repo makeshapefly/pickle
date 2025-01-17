abcee92ee7b3a98eac1aeda12758ecc085dffbdc
tony turner <tony.turner@carbonplace.com>
2025-01-17 09:17:12 +0000

    Initialised

---
+++ backend-admin/app/page.js
@@ -0,0 +1,23 @@
+import Layout from "@/components/layout/Layout"
+import Goal1 from "@/components/sections/Goal1"
+export default function Home() {
+
+    return (
+        <>
+
+            <Layout>
+                <div className="tf-section-4 mb-30">
+                    <Goal1 />
+                </div>
+                <div className="tf-section-8 mb-30">
+                </div>
+                <div className="tf-section-8 mb-30">
+                    <div className="tf-section-2">
+                    </div>
+                </div>
+                <div className="tf-section-2">
+                </div>
+            </Layout>
+        </>
+    )
+}
