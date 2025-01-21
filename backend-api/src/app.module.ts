76783f372df4e743651532ca2a02dd30a10f1c7a
tony turner <tony.turner@carbonplace.com>
2025-01-21 10:32:12 +0000

    vercel config

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -13,11 +13,12 @@ import { MemberModule } from './member.module';
       //url: process.env.POSTGRES_URL,
       host: 'ep-late-rain-a2bj3tfd-pooler.eu-central-1.aws.neon.tech',
       port: 5432,
+      database: 'neondb',
       username: 'neondb_owner',
       password: 'coEyiTwD9P2O',
       "ssl": true,
       entities: [Member],
-      synchronize: true,
+      synchronize: false,
     }),
     TypeOrmModule.forFeature([Member])
   ],
