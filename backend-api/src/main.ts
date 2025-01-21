4569b51ad29f0820ecd595a75edc036bd38b9de9
tony turner <tony.turner@carbonplace.com>
2025-01-21 11:41:20 +0000

    vercel config

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -4,9 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://86.148.56.156:8081',
+    origin: ['http://86.148.56.156:8081', 'http://86.148.56.156', '*'],
     credentials: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: [
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ]
   });
   await app.listen(process.env.PORT ?? 3000);
 }
